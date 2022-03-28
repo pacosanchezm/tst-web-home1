49e2b8dfa2ddf6c679e32836152b39c47921d294
pacosanchezm <paco.sanchezm@gmail.com>
2022-03-28 02:00:52 -0600

    drop lets

--- src/appi.jsx
+++ src/appi.jsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>The Same Tone</h1>
-      <h2>Lets change the tone, lets save the future.</h2>
+      <h2>Change the tone, save the future.</h2>
     </div>
   );
 }
